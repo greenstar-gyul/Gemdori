b69bd2d9ccfdd5afa879aadd0f632c4a13d3e2da
greenstar-gyul <wasdzxcv@naver.com>
2025-04-17 17:04:54 +0900

    게임 상세 화면

---
+++ Gemdori/src/main/java/com/gemdori/vo/GameVO.java
@@ -0,0 +1,38 @@
+package com.gemdori.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class GameVO {
+    private String gameCode;
+    private String gameTitle;
+    private String gameDesc;
+    private String gameContents;
+    private String gameDeveloper;
+    private String gamePublisher;
+    private Date publishingDate;
+    private int gamePrice;
+    private int gameSalePrice;
+    private int discountPer;
+    private String languageSup;
+    private int requiredAge;
+    private String legalNotice;
+    private int freeGame;
+    private String website;
+    private String gameGenre;
+    private String gameCategory;
+    private String gameTag;
+    private String gameMainImage;
+    private String ingameImages;
+    private String gameSysReq;
+    private String gameSysReqR;
+    private int dlcGame;
+
+    // 평점 컬럼 (7번 기능에서 사용)
+    private Double avgRating;
+
+    // DLC 게임일 경우, 원본 게임 코드 (원본 게임은 NULL)
+    private String parentGame;
+} 
