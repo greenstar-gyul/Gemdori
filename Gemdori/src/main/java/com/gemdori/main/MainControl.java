ab2a33a7e8d3e6b811eda20ce0629432b6f87c7a
geniusIN <rlaghddls9309@gmail.com>
2025-04-17 17:40:21 +0900

    d
    d

--- Gemdori/src/main/java/com/gemdori/main/MainControl.java
+++ Gemdori/src/main/java/com/gemdori/main/MainControl.java
@@ -1,19 +1,32 @@
 package com.gemdori.main;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import com.gemdori.common.Control;
+import com.gemdori.mapper.GemdoriGameMapper;
+import com.gemdori.vo.GemdoriGameVO;
+import com.gemdori.common.MybatisSessionFactory;
 
 public class MainControl implements Control {
 
-	@Override
-	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		req.getRequestDispatcher("main/main.tiles").forward(req, resp);
-	}
+    @Override
+    public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SqlSessionFactory factory = MybatisSessionFactory.getInstance();
+        try (SqlSession session = factory.openSession(true)) {
+            GemdoriGameMapper mapper = session.getMapper(GemdoriGameMapper.class);
+            List<GemdoriGameVO> gameList = mapper.selectAllGames();
+            
+            req.setAttribute("gameList", gameList);
+        }
 
+        req.getRequestDispatcher("main/main.tiles").forward(req, resp);
+    }
 }
