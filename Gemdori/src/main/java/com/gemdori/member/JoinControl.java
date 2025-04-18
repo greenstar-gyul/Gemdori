17c9bcc39dd1f45949ea445a7c397941f0cc3147
geniusIN <rlaghddls9309@gmail.com>
2025-04-18 17:41:06 +0900

    Revert "Merge branch 'main' into 홍인"
    This reverts commit 1aba0e1c605467fb8f4008ef254b6fa69a69618d, reversing
    changes made to d2a321e55834272a6a8c577b73bb43ef38371385.

--- Gemdori/src/main/java/com/gemdori/member/JoinControl.java
+++ Gemdori/src/main/java/com/gemdori/member/JoinControl.java
@@ -12,7 +12,6 @@ import com.gemdori.common.Control;
 import com.gemdori.member.service.UserService;
 import com.gemdori.member.service.UserServiceImpl;
 import com.gemdori.member.vo.UserProfileVO;
-import com.gemdori.member.vo.UserSecurityVO;
 import com.gemdori.member.vo.UserSignVO;
 
 public class joinControl implements Control {
@@ -20,7 +19,7 @@ public class joinControl implements Control {
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/json;charset=utf-8");
-		// 1. 파라미터 수집.
+		// 1. 파라미터 수집
 		String userId = req.getParameter("userId");
 		String userPw = req.getParameter("userPw");
 		String userName = req.getParameter("userName");
@@ -60,23 +59,16 @@ public class joinControl implements Control {
             }
 
             boolean isProfileSuccess = service.addUserProfile(profileVO);
-            // 보안 테이블 insert
-         	UserSecurityVO securityVO = new UserSecurityVO();
-         	securityVO.setUserCode(signVO.getUserCode());
-         	securityVO.setLoginFailCount(0); // 기본값
-         	securityVO.setIsLocked(1);       // 잠김 아님
 
-         	boolean isSecuritySuccess = service.addUserSecurity(securityVO);
-            
-			if (isProfileSuccess && isSecuritySuccess) {
-				resp.setContentType("text/html; charset=UTF-8");
-				PrintWriter out = resp.getWriter();
-				out.println("<script>");
-				out.println("alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');");
-				out.println("location.href='loginForm.do';");
-				out.println("</script>");
-				return;
-			}
+            if (isProfileSuccess) {
+                resp.setContentType("text/html; charset=UTF-8");
+                PrintWriter out = resp.getWriter();
+                out.println("<script>");
+                out.println("alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');");
+                out.println("location.href='loginForm.do';");
+                out.println("</script>");
+                return;
+            }
         }
 
         // 실패 시
